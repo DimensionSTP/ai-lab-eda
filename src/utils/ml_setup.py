aeeedb48ce56ca88d15d25c2a00de6fd5089741f
Joohyung-Han <65501090+Joohyung-Han@users.noreply.github.com>
2024-01-23 03:51:09 +0900

    update: update function name and type hint

--- src/utils/ml_setup.py
+++ src/utils/ml_setup.py
@@ -1,18 +1,21 @@
+from typing import Union
+
 from omegaconf import DictConfig
 from hydra.utils import instantiate
 
 from ..dataset_modules.dataset import DiabetesDataset
-from ..architecture_modules.basic_archimodule import BasicClassifierModule
+from ..architecture_modules.lgbm_archimodule import LGBMClassifierModule
+from ..architecture_modules.xgb_archimodule import XGBClassifierModule
 
 
 class MLSetUp:
     def __init__(self, config: DictConfig):
         self.config = config
 
-    def get_diabetes_dataset(self) -> DiabetesDataset:
+    def get_dataset(self) -> DiabetesDataset:
         diabetes_dataset: DiabetesDataset = instantiate(self.config.dataset_module)
         return diabetes_dataset
 
-    def get_basic_classifier(self) -> BasicClassifierModule:
-        basic_archimodule: BasicClassifierModule = instantiate(self.config.architecture_module)
-        return basic_archimodule
+    def get_architecture_module(self) -> Union[LGBMClassifierModule, XGBClassifierModule]:
+        architecture_module: Union[LGBMClassifierModule, XGBClassifierModule] = instantiate(self.config.architecture_module)
+        return architecture_module
